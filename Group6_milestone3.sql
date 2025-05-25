f7236d7e725293e8d677abb4b97b79e10e064ce7
jssison <jssison5@up.edu.ph>
2025-05-26 00:50:46 +0800

    refactor: uncommented queries

--- Group6_milestone3.sql
+++ Group6_milestone3.sql
@@ -113,199 +113,199 @@ INSERT INTO member_pays_fee VALUES
 
 -- Reports
 
--- -- #1
--- --Member details per member of an org
--- CREATE VIEW members_by_details AS
--- 	SELECT mem.mem_id AS `Membership ID`, 
--- 	CASE
---         WHEN mem.mname IS NOT NULL AND mem.mname != ''
---             THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
---         ELSE CONCAT(mem.fname, ' ', mem.lname)
---     END AS `Full Name`, 
--- 	orgmem.org_role AS `Role`,
--- 	orgmem.mem_status AS `Status`, 
--- 	mem.gender AS `Gender`, 
--- 	mem.degprog AS `Degree Program`, 
--- 	mem.batch_year AS `Batch year`, 
--- 	orgmem.committee AS `Committee`
--- 	FROM member AS mem JOIN organization_has_member AS orgmem ON 
--- 	mem.mem_id = orgmem.mem_id;
+-- #1
+--Member details per member of an org
+CREATE VIEW members_by_details AS
+	SELECT mem.mem_id AS `Membership ID`, 
+	CASE
+        WHEN mem.mname IS NOT NULL AND mem.mname != ''
+            THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
+        ELSE CONCAT(mem.fname, ' ', mem.lname)
+    END AS `Full Name`, 
+	orgmem.org_role AS `Role`,
+	orgmem.mem_status AS `Status`, 
+	mem.gender AS `Gender`, 
+	mem.degprog AS `Degree Program`, 
+	orgmem.batch_year AS `Batch year`, 
+	orgmem.committee AS `Committee`
+	FROM member AS mem JOIN organization_has_member AS orgmem ON 
+	mem.mem_id = orgmem.mem_id;
 
--- -- #2
--- --Display members with unpaid fees based on payment status
--- CREATE VIEW member_with_unpaid_membership_fees AS
--- 	SELECT 
--- 		m.mem_id AS `Membership ID`,
--- 		CONCAT(m.fname, ' ', IFNULL(m.mname, ''), IF(m.mname IS NOT NULL, ' ', ''), m.lname) AS `Full Name`,
--- 		m.degprog AS `Degree Program`,
--- 		m.gender AS `Gender`,
--- 		ohm.org_id AS `Organization`,
--- 		mpf.academic_year AS `Academic Year`,
--- 		mpf.semester AS `Semester`
--- 	FROM member m
--- 	JOIN organization_has_member ohm
--- 		ON m.mem_id = ohm.mem_id
--- 	JOIN member_pays_fee mpf
--- 		ON m.mem_id = mpf.mem_id
--- 	WHERE mpf.payment_status = 'Not Paid';
-
-
--- -- #3
--- --Check payment status of each payment made by members 
--- --if it is unpaid, include them on the view
--- CREATE VIEW unpaid_member_fees AS	
--- 	SELECT fee.fee_refnum AS `Fee reference number`, 
--- 	fee.category AS `Category`, 
--- 	fee.due_date AS `Due date`, 
--- 	fee.amount AS `Amount`, 
--- 	org.org_name AS `Organization Name`, 
--- 	mem_fee.academic_year AS `Academic Year`, 
--- 	mem_fee.semester AS `Semester`, 
--- 	mem_fee.payment_status AS `Payment Status` 
--- 	FROM  member AS mem JOIN member_pays_fee AS mem_fee 
--- 	ON mem.mem_id = mem_fee.mem_id
--- 	JOIN fee ON mem_fee.fee_refnum = fee.fee_refnum
--- 	JOIN organization AS org
--- 	ON fee.org_id = org.org_id
--- 	WHERE mem_fee.payment_status = 'Not Paid';
-
--- -- #4
--- --Check member's committee if they are in the executive committee
--- CREATE VIEW executive_committee_members AS
--- 	SELECT mem.mem_id AS `Membership ID`,
--- 	CASE
---         WHEN mem.mname IS NOT NULL AND mem.mname != ''
---             THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
---         ELSE CONCAT(mem.fname, ' ', mem.lname)
---     END AS `Full Name`, 
--- 	org.org_name AS `Organization name`, 
--- 	orgmem.academic_year AS `Academic year`
--- 	FROM member AS mem JOIN organization_has_member AS orgmem
--- 	ON mem.mem_id = orgmem.mem_id 
--- 	JOIN organization AS org 
--- 	ON orgmem.org_id = org.org_id
--- 	WHERE orgmem.committee = 'Executive';
-
--- -- #5
--- --Display presidents/specified role of a given org per academic year 
--- CREATE VIEW presidents AS
--- 	SELECT mem.mem_id AS `Membership ID`,
--- 	CASE
---         WHEN mem.mname IS NOT NULL AND mem.mname != ''
---             THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
---         ELSE CONCAT(mem.fname, ' ', mem.lname)
---     END AS `Full Name`,  
--- 	orgmem.org_role AS `Role`,
--- 	org.org_name AS `Organization name`,
--- 	orgmem.academic_year AS `Academic year`
--- 	FROM member AS mem JOIN organization_has_member AS orgmem
--- 	ON mem.mem_id = orgmem.mem_id
--- 	JOIN organization AS org
--- 	ON orgmem.org_id = org.org_id
--- 	WHERE orgmem.org_role = 'President'
--- 	ORDER BY orgmem.academic_year DESC;
-
--- -- #6
--- --Display late payments based on payment date and due date
--- CREATE VIEW late_payments_view AS
--- 	SELECT 
--- 		m.mem_id AS `Membership ID`,
--- 		CONCAT(m.fname, ' ', IFNULL(m.mname, ''), IF(m.mname IS NOT NULL, ' ', ''), m.lname) AS `Full Name`,
--- 		m.degprog AS `Degree Program`,
--- 		m.gender AS `Gender`,
--- 		ohm.org_id AS `Organization ID`,
--- 		mpf.academic_year AS `Academic Year`,
--- 		mpf.semester AS `Semester`,
--- 		f.fee_refnum AS `Fee Reference`,
--- 		f.due_date AS `Due Date`,
--- 		mpf.date_of_payment AS `Date of Payment`,
--- 		mpf.payment_status AS `Payment Status`
--- 	FROM member m
--- 	JOIN organization_has_member ohm
--- 		ON m.mem_id = ohm.mem_id
--- 	JOIN member_pays_fee mpf
--- 		ON m.mem_id = mpf.mem_id
--- 	JOIN fee f
--- 		ON mpf.fee_refnum = f.fee_refnum
--- 	WHERE mpf.date_of_payment > f.due_date;
-
--- -- #7
--- --Display active vs inactive member percantage based on member status
--- CREATE VIEW active_vs_inactive_percentage AS 
--- 	SELECT 
--- 		academic_year,
--- 		semester,
--- 		COUNT(*) AS total_members,
--- 		SUM(mem_status = 'Active') AS active_count,
--- 		SUM(mem_status = 'Inactive') AS inactive_count,
--- 		ROUND((SUM(mem_status = 'Active') / COUNT(*)) * 100, 2) AS active_percentage,
--- 		ROUND((SUM(mem_status = 'Inactive') / COUNT(*)) * 100, 2) AS inactive_percentage
--- 	FROM 
--- 		organization_has_member
--- 	WHERE 
--- 		organization_id = organization_id
--- 	GROUP BY 
--- 		academic_year, semester
--- 	ORDER BY 
--- 		academic_year DESC, semester DESC;
-
--- -- #8
--- --Display alumni members of a given org
--- CREATE VIEW alumni_members AS
--- SELECT mem.mem_id AS `Membership ID`,
---     CASE
---         WHEN mem.mname IS NOT NULL AND mem.mname != ''
---             THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
---         ELSE CONCAT(mem.fname, ' ', mem.lname)
---     END AS `Full Name`,
---     org.org_name AS `Organization Name`
--- FROM member AS mem JOIN organization_has_member AS orgmem 
--- ON mem.mem_id = orgmem.mem_id 
--- JOIN organization AS org 
--- ON orgmem.org_id = org.org_id 
--- WHERE orgmem.mem_status = 'Alumni';
+-- #2
+--Display members with unpaid fees based on payment status
+CREATE VIEW member_with_unpaid_membership_fees AS
+	SELECT 
+		m.mem_id AS `Membership ID`,
+		CONCAT(m.fname, ' ', IFNULL(m.mname, ''), IF(m.mname IS NOT NULL, ' ', ''), m.lname) AS `Full Name`,
+		m.degprog AS `Degree Program`,
+		m.gender AS `Gender`,
+		ohm.org_id AS `Organization`,
+		mpf.academic_year AS `Academic Year`,
+		mpf.semester AS `Semester`
+	FROM member m
+	JOIN organization_has_member ohm
+		ON m.mem_id = ohm.mem_id
+	JOIN member_pays_fee mpf
+		ON m.mem_id = mpf.mem_id
+	WHERE mpf.payment_status = 'Not Paid';
 
 
--- -- #9
--- --Display total amount of paid and unpaid fees
--- CREATE VIEW total_unpaid AS
--- SELECT o.org_name,
---     -- Total Paid Amount (before or on today)
---     COALESCE(SUM(
---         CASE
---             WHEN mpf.payment_status = 'Paid' AND mpf.date_of_payment <= CURRENT_DATE THEN f.amount
---             ELSE 0 END), 0) AS total_paid_amount,    COALESCE(SUM(
---         CASE 
---             WHEN f.due_date <= CURRENT_DATE AND 
---                  (mpf.payment_status = 'Not Paid' OR mpf.date_of_payment > CURRENT_DATE)
---             THEN f.amount
---             ELSE 0 END), 0) AS total_unpaid_amount
+-- #3
+--Check payment status of each payment made by members 
+--if it is unpaid, include them on the view
+CREATE VIEW unpaid_member_fees AS	
+	SELECT fee.fee_refnum AS `Fee reference number`, 
+	fee.category AS `Category`, 
+	fee.due_date AS `Due date`, 
+	fee.amount AS `Amount`, 
+	org.org_name AS `Organization Name`, 
+	mem_fee.academic_year AS `Academic Year`, 
+	mem_fee.semester AS `Semester`, 
+	mem_fee.payment_status AS `Payment Status` 
+	FROM  member AS mem JOIN member_pays_fee AS mem_fee 
+	ON mem.mem_id = mem_fee.mem_id
+	JOIN fee ON mem_fee.fee_refnum = fee.fee_refnum
+	JOIN organization AS org
+	ON fee.org_id = org.org_id
+	WHERE mem_fee.payment_status = 'Not Paid';
 
--- FROM organization o
--- JOIN fee f ON o.org_id = f.org_id
--- JOIN member_pays_fee mpf ON f.fee_refnum = mpf.fee_refnum
--- GROUP BY o.org_name;
+-- #4
+--Check member's committee if they are in the executive committee
+CREATE VIEW executive_committee_members AS
+	SELECT mem.mem_id AS `Membership ID`,
+	CASE
+        WHEN mem.mname IS NOT NULL AND mem.mname != ''
+            THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
+        ELSE CONCAT(mem.fname, ' ', mem.lname)
+    END AS `Full Name`, 
+	org.org_name AS `Organization name`, 
+	orgmem.academic_year AS `Academic year`
+	FROM member AS mem JOIN organization_has_member AS orgmem
+	ON mem.mem_id = orgmem.mem_id 
+	JOIN organization AS org 
+	ON orgmem.org_id = org.org_id
+	WHERE orgmem.committee = 'Executive';
+
+-- #5
+--Display presidents/specified role of a given org per academic year 
+CREATE VIEW presidents AS
+	SELECT mem.mem_id AS `Membership ID`,
+	CASE
+        WHEN mem.mname IS NOT NULL AND mem.mname != ''
+            THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
+        ELSE CONCAT(mem.fname, ' ', mem.lname)
+    END AS `Full Name`,  
+	orgmem.org_role AS `Role`,
+	org.org_name AS `Organization name`,
+	orgmem.academic_year AS `Academic year`
+	FROM member AS mem JOIN organization_has_member AS orgmem
+	ON mem.mem_id = orgmem.mem_id
+	JOIN organization AS org
+	ON orgmem.org_id = org.org_id
+	WHERE orgmem.org_role = 'President'
+	ORDER BY orgmem.academic_year DESC;
+
+-- #6
+--Display late payments based on payment date and due date
+CREATE VIEW late_payments_view AS
+	SELECT 
+		m.mem_id AS `Membership ID`,
+		CONCAT(m.fname, ' ', IFNULL(m.mname, ''), IF(m.mname IS NOT NULL, ' ', ''), m.lname) AS `Full Name`,
+		m.degprog AS `Degree Program`,
+		m.gender AS `Gender`,
+		ohm.org_id AS `Organization ID`,
+		mpf.academic_year AS `Academic Year`,
+		mpf.semester AS `Semester`,
+		f.fee_refnum AS `Fee Reference`,
+		f.due_date AS `Due Date`,
+		mpf.date_of_payment AS `Date of Payment`,
+		mpf.payment_status AS `Payment Status`
+	FROM member m
+	JOIN organization_has_member ohm
+		ON m.mem_id = ohm.mem_id
+	JOIN member_pays_fee mpf
+		ON m.mem_id = mpf.mem_id
+	JOIN fee f
+		ON mpf.fee_refnum = f.fee_refnum
+	WHERE mpf.date_of_payment > f.due_date;
+
+-- #7
+--Display active vs inactive member percantage based on member status
+CREATE VIEW active_vs_inactive_percentage AS 
+	SELECT 
+		academic_year,
+		semester,
+		COUNT(*) AS total_members,
+		SUM(mem_status = 'Active') AS active_count,
+		SUM(mem_status = 'Inactive') AS inactive_count,
+		ROUND((SUM(mem_status = 'Active') / COUNT(*)) * 100, 2) AS active_percentage,
+		ROUND((SUM(mem_status = 'Inactive') / COUNT(*)) * 100, 2) AS inactive_percentage
+	FROM 
+		organization_has_member
+	WHERE 
+		organization_id = organization_id
+	GROUP BY 
+		academic_year, semester
+	ORDER BY 
+		academic_year DESC, semester DESC;
+
+-- #8
+--Display alumni members of a given org
+CREATE VIEW alumni_members AS
+SELECT mem.mem_id AS `Membership ID`,
+    CASE
+        WHEN mem.mname IS NOT NULL AND mem.mname != ''
+            THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
+        ELSE CONCAT(mem.fname, ' ', mem.lname)
+    END AS `Full Name`,
+    org.org_name AS `Organization Name`
+FROM member AS mem JOIN organization_has_member AS orgmem 
+ON mem.mem_id = orgmem.mem_id 
+JOIN organization AS org 
+ON orgmem.org_id = org.org_id 
+WHERE orgmem.mem_status = 'Alumni';
 
 
--- -- #10
--- -- Display member details with the highest debt of a given org for a given sem
--- CREATE VIEW highest_debt 
--- 	SELECT m.mem_id,
--- 	m.fname,
--- 	m.lname,
--- 	SUM(CASE
--- 	WHEN mpf.payment_status != 'Paid' THEN f.amount ELSE 0 END) AS unpaid_amount
--- 	FROM member m
--- 	JOIN member_pays_fee mpf ON m.mem_id = mpf.mem_id
--- 	JOIN fee f ON mpf.fee_refnum = f.fee_refnum
--- 	WHERE f.org_id = given_organization_id
--- 	And mpf.semester = given_semester
--- 	GROUP BY m.mem_id, m.fname,m.lname
--- 	ORDER BY unpaid_amount DESC	;	
+-- #9
+--Display total amount of paid and unpaid fees
+CREATE VIEW total_unpaid AS
+SELECT o.org_name,
+    -- Total Paid Amount (before or on today)
+    COALESCE(SUM(
+        CASE
+            WHEN mpf.payment_status = 'Paid' AND mpf.date_of_payment <= CURRENT_DATE THEN f.amount
+            ELSE 0 END), 0) AS total_paid_amount,    COALESCE(SUM(
+        CASE 
+            WHEN f.due_date <= CURRENT_DATE AND 
+                 (mpf.payment_status = 'Not Paid' OR mpf.date_of_payment > CURRENT_DATE)
+            THEN f.amount
+            ELSE 0 END), 0) AS total_unpaid_amount
 
--- -- Delete data
--- DELETE FROM organization_has_member;
--- DELETE FROM member_pays_fee;
--- DELETE FROM fee;
--- DELETE FROM member;
--- DELETE FROM organization;
+FROM organization o
+JOIN fee f ON o.org_id = f.org_id
+JOIN member_pays_fee mpf ON f.fee_refnum = mpf.fee_refnum
+GROUP BY o.org_name;
+
+
+-- #10
+-- Display member details with the highest debt of a given org for a given sem
+CREATE VIEW highest_debt 
+	SELECT m.mem_id,
+	m.fname,
+	m.lname,
+	SUM(CASE
+	WHEN mpf.payment_status != 'Paid' THEN f.amount ELSE 0 END) AS unpaid_amount
+	FROM member m
+	JOIN member_pays_fee mpf ON m.mem_id = mpf.mem_id
+	JOIN fee f ON mpf.fee_refnum = f.fee_refnum
+	WHERE f.org_id = given_organization_id
+	And mpf.semester = given_semester
+	GROUP BY m.mem_id, m.fname,m.lname
+	ORDER BY unpaid_amount DESC	;	
+
+-- Delete data
+DELETE FROM organization_has_member;
+DELETE FROM member_pays_fee;
+DELETE FROM fee;
+DELETE FROM member;
+DELETE FROM organization;

6b695d28c55dc835bb6786edc17c3275cf1aff01
jssison <jssison5@up.edu.ph>
2025-05-26 00:11:46 +0800

    refactor: used imports for creating and populating tables

--- admin/connect.py
+++ admin/connect.py
@@ -1,5 +1,7 @@
 import mariadb
 import sys
+import create
+import populate
 
 # Connect to mariadb
 try:
@@ -18,89 +20,20 @@ except mariadb.Error as e:
 cur = conn.cursor()
 
 #Create tables
-createOrg = """
-CREATE TABLE IF NOT EXISTS organization(
-	org_id INT AUTO_INCREMENT,  
-	org_name VARCHAR(50) NOT NULL, 
-	org_username VARCHAR(50) NOT NULL,
-	org_password VARCHAR(50) NOT NULL,
-	CONSTRAINT organization_org_id_pk PRIMARY KEY (org_id), 
-	CONSTRAINT organization_org_username_uk UNIQUE (org_username) 
-);
-"""
+create.create_org(cur, conn)
+create.create_mem(cur, conn)
+create.create_fee(cur, conn)
+create.create_orghasmem(cur, conn)
+create.create_mempaysfee(cur, conn)
 
-#Create member table
-createMem = """
-CREATE TABLE IF NOT EXISTS member(
-    mem_id INT AUTO_INCREMENT, 
-    mem_uname VARCHAR(50) NOT NULL,
-    mem_pword VARCHAR(50) NOT NULL,
-    fname VARCHAR(50) NOT NULL,
-    mname VARCHAR(50),
-    lname VARCHAR(50) NOT NULL,
-    degprog VARCHAR(50) NOT NULL,
-    gender CHAR(1) NOT NULL,
-    CONSTRAINT member_mem_id_pk PRIMARY KEY(mem_id), 
-    CONSTRAINT member_mem_uname_uk UNIQUE (mem_uname) 
-);
-"""
-
-#Create fee table
-createFee = """
-CREATE TABLE IF NOT EXISTS fee(
-    fee_refnum INT(10),
-    category VARCHAR(20) NOT NULL, 
-    due_date DATE NOT NULL,
-    amount INT NOT NULL,
-    org_id INT(20) NOT NULL,
-    CONSTRAINT fee_fee_refnum_pk PRIMARY KEY(fee_refnum), 
-    CONSTRAINT fee_org_id_fk FOREIGN KEY(org_id) REFERENCES organization(org_id) 
-);
-"""
-
-#Create org_has_mem table
-createOrgHasMem = """
-CREATE TABLE IF NOT EXISTS organization_has_member(
-    org_id INT NOT NULL, 
-    mem_id INT NOT NULL,
-    academic_year VARCHAR(9) NOT NULL,
-    committee VARCHAR(50) NOT NULL,
-    semester VARCHAR(20) NOT NULL,
-    org_role VARCHAR(50) NOT NULL,
-    batch_year YEAR NOT NULL,
-    batch_name VARCHAR(50) NOT NULL,
-    mem_status VARCHAR(10) NOT NULL,
-    CONSTRAINT organization_has_member_org_id_pk PRIMARY KEY (org_id, mem_id, academic_year, committee,
-    semester, org_role, batch_year, mem_status), 
-    CONSTRAINT organization_has_member_org_id_fk FOREIGN KEY (org_id) REFERENCES organization(org_id), 
-    CONSTRAINT organization_has_member_mem_id_fk FOREIGN KEY (mem_id) REFERENCES member(mem_id) 
-);
-"""
-
-#Create member_pays_fee table
-createMemPaysFee = """
-CREATE TABLE IF NOT EXISTS member_pays_fee (
-    mem_id INT NOT NULL, 
-    fee_refnum INT NOT NULL,
-    academic_year VARCHAR(9) NOT NULL,
-    semester VARCHAR(20) NOT NULL,
-    date_of_payment DATE,
-    payment_status VARCHAR(20) NOT NULL,
-	CONSTRAINT member_pays_fee_mem_id_pk PRIMARY KEY (mem_id, fee_refnum, academic_year, semester, date_of_payment, payment_status),
-    CONSTRAINT member_pays_fee_mem_id_fk FOREIGN KEY (mem_id) REFERENCES member(mem_id), 
-    CONSTRAINT member_pays_fee_fee_refnum_fk FOREIGN KEY (fee_refnum) REFERENCES fee(fee_refnum)
-);
-"""
-
-#Execute create table commands
-try:
-    cur.execute(createOrg)
-    cur.execute(createMem)
-    cur.execute(createFee)
-    cur.execute(createOrgHasMem)
-    cur.execute(createMemPaysFee)
-except mariadb.Error as e:
-    print(f"Error creating tables: {e}")
+#Populate database
+populate.insert_to_org(cur, conn)
+populate.insert_to_mem(cur, conn)
+populate.insert_to_fee(cur, conn)
+populate.insert_to_orghasmem(cur, conn)
+populate.insert_to_mempaysfee(cur, conn)
 
 #Commit changes
 conn.commit()
+
+print('Successfully created and populated tables.')

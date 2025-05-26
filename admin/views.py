af4fe66490c1f017e3c291f4cc77b865142e8ae6
shrgastardo <shrgastardo@gmail.com>
2025-05-26 19:31:49 +0800

    Update views.py

--- admin/views.py
+++ admin/views.py
@@ -7,7 +7,7 @@ import mariadb
 # conn (in connect.py) is the same as connection (in functions)
 
 #View members by the specified ordering type
-def view_members_by(cursor, connection, order):
+def view_members_by(cursor, connection, order, org_id):
     #Allowable ordering types
     ordering_types = ['`Role`', '`Status`', '`Gender`', '`Degree Program`', '`Batch year`', '`Committee`']
 
@@ -30,8 +30,9 @@ def view_members_by(cursor, connection, order):
                 orgmem.committee AS `Committee`
                 FROM member AS mem JOIN organization_has_member AS orgmem ON 
                 mem.mem_id = orgmem.mem_id
+                WHERE orgmem.org_id = ?
                 ORDER BY {order};
-            """)
+            """, (org_id,))
         except mariadb.Error as e:
             print(f'Error generating view {e}')
         
@@ -43,9 +44,26 @@ def view_members_by(cursor, connection, order):
         print('Invalid ordering type.')
 
 #2
-def view_unpaid_members(cursor, connection, sem, acad_yr):
+def view_unpaid_members(cursor, connection, org_id, sem, acad_yr):
     try:
-        cursor.execute()
+        cursor.execute(f"""
+        CREATE VIEW member_with_unpaid_membership_fees AS
+        SELECT 
+            m.mem_id AS `Membership ID`,
+            CONCAT(m.fname, ' ', IFNULL(m.mname, ''), IF(m.mname IS NOT NULL, ' ', ''), m.lname) AS `Full Name`,
+            m.degprog AS `Degree Program`,
+            m.gender AS `Gender`,
+            ohm.org_id AS `Organization`,
+            mpf.academic_year AS `Academic Year`,
+            mpf.semester AS `Semester`
+        FROM member m
+        JOIN organization_has_member ohm
+            ON m.mem_id = ohm.mem_id
+        JOIN member_pays_fee mpf
+            ON m.mem_id = mpf.mem_id
+        WHERE mpf.payment_status = 'Not Paid' AND ohm.org_id = ? AND mpf.semester = ? AND mpf.academic_year = ?;
+        """, (org_id, sem, acad_yr))
+
     except mariadb.Error as e:
         print(f'Error generating view {e}')
 
@@ -61,9 +79,24 @@ def view_unpaid_fees(cursor, connection, member):
     connection.commit()
 
 #4
-def view_executive_members(cursor, connection, org, sem, acad_yr):
+def view_executive_members(cursor, connection, org_id, acad_yr):
     try:
-        cursor.execute()
+        cursor.execute(f"""CREATE VIEW executive_committee_members AS
+	SELECT mem.mem_id AS `Membership ID`,
+	CASE
+        WHEN mem.mname IS NOT NULL AND mem.mname != ''
+            THEN CONCAT(mem.fname, ' ', mem.mname, ' ', mem.lname)
+        ELSE CONCAT(mem.fname, ' ', mem.lname)
+    END AS `Full Name`, 
+	org.org_name AS `Organization name`, 
+	orgmem.academic_year AS `Academic year`
+	FROM member AS mem JOIN organization_has_member AS orgmem
+	ON mem.mem_id = orgmem.mem_id 
+	JOIN organization AS org 
+	ON orgmem.org_id = org.org_id
+	WHERE orgmem.committee = 'Executive' AND org.org_id = ? AND orgmem.academic_year = ?;
+    """, (org_id, acad_yr,))
+        
     except mariadb.Error as e:
         print(f'Error generating view {e}')
 

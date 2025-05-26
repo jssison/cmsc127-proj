791139aa17719b767d60df1cad0f690952add9ab
shrgastardo <shrgastardo@gmail.com>
2025-05-26 19:32:25 +0800

    Add files via upload

---
+++ org_login.py
@@ -0,0 +1,65 @@
+import connect
+import login
+import views
+
+def org_login():
+    print('\n========== VIEW ==========')
+    print('[1] Organization Members')
+    print('[2] Organization Fees')
+    print('[3] Back to Main Menu')
+    choice = input('Enter choice: ')
+    
+    org_id = login.org_id
+
+    match choice:
+        case '1':
+            print('\n[1] Members of the Organization')
+            print('[2] Executive Members')
+            print('[3] Alumni Members')
+            print('[4] List of Past and Present Presidents')
+            print('[5] Back to View Menu')
+            choice1 = input('Enter choice: ')
+
+            match choice1:
+                case '1':
+                    views.view_members_by(connect.cur, connect.conn, org_id)
+
+                case '2':
+                    acad_year = input("Enter academic year (e.g., 2024–2025): ")
+                    views.view_executive_members(connect.cur, connect.conn, org_id, acad_year)
+
+                case '5':
+                    org_login()
+                
+                case _:
+                    print("Invalid choice.")
+        
+        case '2':
+            print('\n[1] List of Unpaid Members')
+            print('[2] Late Payments by all Members')
+            print('[3] Highest Member/s with Unpaid Fees')
+            print('[4] Total Unpaid and Paid Fees')
+            print('[5] Back to View Menu')
+            choice2 = input('Enter choice: ')
+
+            match choice2:
+                case '1':
+                    semester = input("Enter semester (e.g., 1st Semester): ")
+                    acad_year = input("Enter academic year (e.g., 2024–2025): ")
+                    views.view_unpaid_members(connect.cur, connect.conn, org_id, semester, acad_year)
+                
+                case '2':
+                    semester = input("Enter semester (e.g., 1st Semester): ")
+                    acad_year = input("Enter academic year (e.g., 2024–2025): ")
+                    views.view_late_payments(connect.cur, connect.conn, semester, acad_year)
+                case '5':
+                    org_login()
+                
+                case _:
+                    print("Invalid choice.")
+
+        case '3':
+            login.login()
+
+        case _:
+            print("Invalid choice.")

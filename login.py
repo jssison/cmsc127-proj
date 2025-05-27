434a8d4989dc37b86d964d8fe05417ec59c2342a
shrgastardo <shrgastardo@gmail.com>
2025-05-28 07:05:18 +0800

    Update login.py

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 from admin import connect
-import org_login  # Assuming this contains the org main menu Toplevel
+import org_login 
 import member_login
 import remove_member
 import add_member
